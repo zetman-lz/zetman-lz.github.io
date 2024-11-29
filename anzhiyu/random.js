58345ed694fe1be635a506a4f91b7d28b3d6b6fd
zetman-lz <lz2457199286@gmail.com>
2024-11-29 18:00:33 +0800

    Site updated: 2024-11-29 18:00:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/"];function toRandomPost(){
+var posts=["article/hello-world/","article/hexo-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
