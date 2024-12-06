d24ae3e7f862a60ce8b42e08b9fc9219ffebf7a9
zetman-lz <lz2457199286@gmail.com>
2024-12-06 12:07:42 +0800

    Site updated: 2024-12-06 12:07:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hexo-1/","article/hexo-2/","article/hexo-3/","article/hexo-4/","article/new-blog-post/"];function toRandomPost(){
+var posts=["article/hexo-1/","article/hexo-2/","article/hexo-3/","article/hexo-4/","article/new-blog-post/","article/web-bug-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
