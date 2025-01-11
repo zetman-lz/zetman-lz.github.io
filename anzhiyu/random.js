2da75e5ba81e5b3ac380551c27675400df84a23f
zetman-lz <lz2457199286@gmail.com>
2025-01-12 01:02:44 +0800

    Site updated: 2025-01-12 01:02:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/comfyui-1/","article/comfyui-2/","article/hexo-1/","article/hexo-2/","article/hexo-3/","article/hexo-4/","article/krita-1/","article/new-blog-post/","article/web-bug-1/","article/web-bug-2/"];function toRandomPost(){
+var posts=["article/3x-ui/","article/comfyui-1/","article/comfyui-2/","article/hexo-1/","article/hexo-2/","article/hexo-3/","article/hexo-4/","article/krita-1/","article/new-blog-post/","article/web-bug-1/","article/web-bug-2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
